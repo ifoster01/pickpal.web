629b239ac4a6dd09da4e373d4bea24e9e89733f3
ifoster01 <ifoster41901@gmail.com>
2024-10-07 22:09:00 -0400

    fix: rolled back auth shit didnt work lol

--- src/app/(routes)/auth/layout.tsx
+++ src/app/(routes)/auth/layout.tsx
@@ -20,19 +20,7 @@ export default function ({children}:{children: ReactNode}) {
         if (user) {
             router.push('/authed');
         }
-    }, [user])
-
-    const getURL = () => {
-        let url =
-          process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-          process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-          'http://localhost:3000/'
-        // Make sure to include `https://` when not localhost.
-        url = url.startsWith('http') ? url : `https://${url}`
-        // Make sure to include a trailing `/`.
-        url = url.endsWith('/') ? url : `${url}/`
-        return url
-    }
+    }, [user]);
 
     return (
         <VStack minH='screen' maxW='screen' overflow='auto' pt={8}>
@@ -49,7 +37,7 @@ export default function ({children}:{children: ReactNode}) {
                         const { data, error } = await supabase.auth.signInWithOAuth({
                             provider: "google",
                             options: {
-                                redirectTo: `${getURL()}/auth/callback`,
+                                redirectTo: `${origin}/auth/callback`,
                             },
                         });
 
