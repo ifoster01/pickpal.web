a42a73fadd521f5a8d0cb0ed233ced8fc1b59852
Isaac Foster <ifoster41901@gmail.com>
2024-10-22 16:25:52 -0400

    feat: changed to shorter team name

--- src/app/(routes)/authed/(components)/Picks/NFLGameAccordian.tsx
+++ src/app/(routes)/authed/(components)/Picks/NFLGameAccordian.tsx
@@ -21,42 +21,41 @@ export function NFLGameAccordian({
         } else {
             return 100 / ((Math.abs(odds) + 100))
         }
-        }
+    }
     
-        let odds1_diff = convertToProbability(game.odds1) - convertToProbability(game.team_book_odds ?? 0)
-        let odds2_diff = convertToProbability(game.odds2) - convertToProbability(game.opp_book_odds ?? 0)
-        if (game.team_book_odds === 0 || game.opp_book_odds === 0) {
+    let odds1_diff = convertToProbability(game.odds1) - convertToProbability(game.team_book_odds ?? 0)
+    let odds2_diff = convertToProbability(game.odds2) - convertToProbability(game.opp_book_odds ?? 0)
+    if (game.team_book_odds === 0 || game.opp_book_odds === 0) {
         odds1_diff = 0
         odds2_diff = 0
-        }
-    
-        let value = odds1_diff > 0 ? -1*odds1_diff : odds2_diff
-        if (odds1_diff === 0 && odds2_diff < 0 || odds1_diff < 0 && odds2_diff === 0) {
-        value = 0
-        }
+    }
 
-        const betText = Math.abs(value) > 0.4 ? 'huge' :
-                        Math.abs(value) > 0.3 ? 'large' :
-                        Math.abs(value) > 0.1 ? 'medium' :
-                        Math.abs(value) > 0.05 ? 'small' :
-                        'almost no'
+    let value = odds1_diff > 0 ? -1*odds1_diff : odds2_diff
+    if (odds1_diff === 0 && odds2_diff < 0 || odds1_diff < 0 && odds2_diff === 0) {
+        value = 0
+    }
+
+    const betText = Math.abs(value) > 0.4 ? 'huge' :
+                    Math.abs(value) > 0.3 ? 'large' :
+                    Math.abs(value) > 0.1 ? 'medium' :
+                    Math.abs(value) > 0.05 ? 'small' :
+                    'almost no'
 
     return (
         <Accordion.Item key={game.game_id} value={game.game_id}>
             <Accordion.ItemTrigger _focus={{ outline: 'none', boxShadow: 'outline' }}>
                 <HStack w='full' justify='space-around'>
-                    {/* <Avatar src={game.team_pic_url ?? ''} name={game.team_name ?? ''} objectFit={'scale-down'} objectPosition={'top'} /> */}
-                    <Avatar src={game.team_pic_url ?? ''} name={game.team_name ?? ''} />
+                    <Avatar src={game.team_pic_url ?? ''} name={game.team_name?.split(' ').at(-1) ?? ''} />
                     <VStack w='full' gap={0} textAlign='center'>
-                        <Text>{game.team_name}</Text>
+                        <Text>{game.team_name?.split(' ').at(-1)}</Text>
                         <Text>{game.odds1 > 0 ? '+' : ''}{game.odds1}</Text>
                     </VStack>
                     <Text>vs</Text>
                     <VStack w='full' gap={0} textAlign='center'>
-                        <Text>{game.opp_name}</Text>
+                        <Text>{game.opp_name?.split(' ').at(-1)}</Text>
                         <Text>{game.odds2 > 0 ? '+' : ''}{game.odds2}</Text>
                     </VStack>
-                    <Avatar src={game.opp_pic_url ?? ''} name={game.opp_name ?? ''} />
+                    <Avatar src={game.opp_pic_url ?? ''} name={game.opp_name?.split(' ').at(-1) ?? ''} />
                 </HStack>
                 <Accordion.ItemIndicator>
                     <ChevronDownIcon />
@@ -67,24 +66,24 @@ export function NFLGameAccordian({
                     <Text fontWeight={700}>Model Predictions</Text>
                     <HStack w='full' justify='space-around'>
                         <VStack w='full' gap={0} textAlign='center'>
-                            <Text>{game.team_name}</Text>
+                            <Text>{game.team_name?.split(' ').at(-1)}</Text>
                             <Text>{game.odds1 > 0 ? '+' : ''}{game.odds1} ({(convertToProbability(game.odds1) * 100).toFixed()}%)</Text>
                         </VStack>
                         <Text>vs</Text>
                         <VStack w='full' gap={0} textAlign='center'>
-                            <Text>{game.opp_name}</Text>
+                            <Text>{game.opp_name?.split(' ').at(-1)}</Text>
                             <Text>{game.odds2 > 0 ? '+' : ''}{game.odds2} ({(convertToProbability(game.odds2) * 100).toFixed()}%)</Text>
                         </VStack>
                     </HStack>
                     <Text fontWeight={700}>Sports Book</Text>
                     <HStack w='full' justify='space-around'>
                         <VStack w='full' gap={0} textAlign='center'>
-                            <Text>{game.team_name}</Text>
+                            <Text>{game.team_name?.split(' ').at(-1)}</Text>
                             <Text>{game.team_book_odds && game.team_book_odds > 0 ? '+' : ''}{game.team_book_odds ? `${game.team_book_odds} (${(convertToProbability(game.team_book_odds) * 100).toFixed()}%)` : 'N/A'}</Text>
                         </VStack>
                         <Text>vs</Text>
                         <VStack w='full' gap={0} textAlign='center'>
-                            <Text>{game.opp_name}</Text>
+                            <Text>{game.opp_name?.split(' ').at(-1)}</Text>
                             <Text>{game.opp_book_odds && game.opp_book_odds > 0 ? '+' : ''}{game.opp_book_odds ? `${game.opp_book_odds} (${(convertToProbability(game.opp_book_odds) * 100).toFixed()}%)` : 'N/A'}</Text>
                         </VStack>
                     </HStack>
@@ -92,7 +91,7 @@ export function NFLGameAccordian({
                         textAlign='center'
                         marginTop={4}
                     >
-                        { odds1_diff > 0 ? game.team_name + "'s" : game.opp_name + "'s" } odds have a {betText} discrepancy from the book
+                        { odds1_diff > 0 ? game.team_name?.split(' ').at(-1) + "'s" : game.opp_name?.split(' ').at(-1) + "'s" } odds have a {betText} discrepancy from the book
                     </Text> }
                     <Scale value={value} />
                 </VStack>
