dd736c94b3d228aa50fe7673f47dc1d53d2bdf46
Isaac Foster <isaacfoster@Isaacs-MacBook-Pro.local>
2024-12-06 20:38:29 -0500

    feat: create project from supabase template

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 
 interface ProgressProps extends React.HTMLAttributes<HTMLDivElement> {
   value?: number;
