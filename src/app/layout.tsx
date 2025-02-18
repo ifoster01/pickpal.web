761a087948cc37a28b052c3fc403a015576d1ff6
Isaac Foster <ifoster41901@gmail.com>
2025-02-18 08:16:01 -0500

    feat: added microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import type { PropsWithChildren } from 'react'
 import './globals.css'
+import Script from 'next/script'
 import { ThemeProvider } from '@/providers/ThemeProvider';
 
 const defaultUrl = process.env.VERCEL_URL
@@ -17,6 +18,17 @@ const RootLayout = (props: PropsWithChildren) => {
   const { children } = props
   return (
     <html lang="en">
+      <head>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "qbepns771d");
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
